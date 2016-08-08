dacd3ded4492145d67116b9791b463ffbdd41e58
RK <RK@RKC>
2016-08-08 11:36:30 +0530

    added

---
+++ test/pages/com/pages/HomePage.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.pages;
+
+/**
+ * @author RK
+ *
+ */
+public class HomePage {
+
+}
---
+++ test/src/test/java/com/tests/Test_01.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.tests;
+
+import org.testng.annotations.Test;
+
+/**
+ * @author RK
+ *
+ */
+public class Test_01 {
+
+	@Test
+	public void name() {
+		System.out.println("asd");
+	}
+}
